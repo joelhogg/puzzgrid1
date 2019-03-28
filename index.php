bd509c6d37b77d076cb048865230150db9d985bc
joelhogg <43680131+joelhogg@users.noreply.github.com>
2019-03-28 19:57:16 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<style>
+	.title {
+		text-align: center;
+		text-size: 64px;
+	}
+	.links_wrapper {
+		text-align: center;
+	}
+	.links {
+		text-size: 24px;
+		right: 50%;
+	}
+</style>
+
+<html>
+	<h1 class="title">PuzzGrids:</h1>
+	<div class="links_wrapper">
+		<a class="links" href="puzzgrid2.html">Country PuzzGrid</a>
+		<br>
+		<a class="links" href="puzzgrid3.html">PuzzGrid</a>
+	</div>
+	
+</html>
